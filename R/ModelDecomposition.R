4a975dcda4b3394c9ce6f8d67c4a3f945f441c40
harell <harel@loostiger.com>
2020-02-29 15:31:47 +1300

    created abstract class

---
+++ R/ModelDecomposition.R
@@ -0,0 +1,55 @@
+#' @title Abstract Class for Model Decomposition Sub Classes
+#'
+#' @description The class takes an object containing a prediction model and
+#'   decomposes it into its essential parts.
+#'
+#' @field model_object (`?`) A model object to decompose.
+#' @field historical_data (`data.frame`) A data table with the data used to
+#'   create \code{model_object}.
+#' @field new_data (`data.frame`) A data table with the data to predict.
+#' @field role_target (`character`) The name of the target variable in
+#'   \code{model_object}.
+#' @field role_input (`character`) The name of the target variable
+#'   \code{model_object}.
+#'
+#' @export
+#'
+#' @param object (`?`) A model object to decompose.
+#'
+ModelDecomposition <- R6::R6Class(
+    classname = "ModelDecomposition",
+    cloneable = FALSE,
+    lock_objects = TRUE,
+    public = list(
+        # Public Fields --------------------------------------------------------
+        model_object = NULL,
+        historical_data = tibble::tibble(),
+        new_data = tibble::tibble(),
+        role_target = character(),
+        role_input = character(),
+        # Public Methods -------------------------------------------------------
+        #' @description
+        #' Predict method for \code{object}.
+        #' @param newdata (`data.frame`)` A data table in which to look for
+        #'   variables with which to predict.
+        predict_function = function(object, newdata = NULL) predict(object, newdata),
+        #' @description
+        #' Decompose a given object to its essential parts.
+        initialize = function(object){
+            self$model_object <- private$extract_model_object(object)
+            self$historical_data <- private$extract_historical_data(object)
+            self$new_data <- private$extract_new_data(object)
+            self$role_target <- private$extract_role_target(object)
+            self$role_input <- private$extract_role_input(object)
+        }
+    ),
+    private = list(
+        # Private Fields -------------------------------------------------------
+        # Private Methods ------------------------------------------------------
+        extract_model_object = function(obj) stop("I'm a signature function"),
+        extract_historical_data = function(obj) stop("I'm a signature function"),
+        extract_new_data = function(obj) stop("I'm a signature function"),
+        extract_role_target = function(obj) stop("I'm a signature function"),
+        extract_role_input = function(obj) stop("I'm a signature function")
+    )
+)
