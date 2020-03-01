25fd19d39332f7bb89c71d668d75642d30b12494
harell <harel@loostiger.com>
2020-03-01 13:52:05 +1300

    added assertions

--- R/ModelDecomposition.R
+++ R/ModelDecomposition.R
@@ -36,11 +36,18 @@ ModelDecomposition <- R6::R6Class( # nocov start
         #' @description
         #' Decompose a given object to its essential parts.
         initialize = function(object){
+            assert_that <- assertthat::assert_that
+            is.scalar <- assertthat::is.scalar
+
             self$role_target <- private$extract_role_target(object)
             self$role_input <- private$extract_role_input(object)
             self$historical_data <- private$extract_historical_data(object)
             self$new_data <- private$extract_new_data(object)
             self$model_object <- private$extract_model_object(object)
+
+            assert_that(is.character(self$role_target), is.scalar(self$role_target))
+            assert_that(is.character(self$role_input))
+            assert_that(is.data.frame(self$historical_data))
         }
     ),
     private = list(
