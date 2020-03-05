fd8c1370baf69349f139661ea9cee394a88b3ef7
harell <harel@loostiger.com>
2020-03-05 18:04:57 +1300

    display datatable on UI

--- inst/dashboard/server.R
+++ inst/dashboard/server.R
@@ -13,9 +13,32 @@ pkgload::load_all(path = "./package", helpers = FALSE)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
+    # Setup -------------------------------------------------------------------
     caret_model <- Dashboard$funs$get_titanic_glm()
+    model_elements <- caret_model %>% CaretModelDecomposition$new()
 
+    # Observation table -------------------------------------------------------
+    ## Create DT table
+    unseen_observations <-
+        DT::datatable(
+            data = model_elements$data,
+            extensions = "Scroller",
+            style = "bootstrap",
+            class = "compact",
+            selection = list(mode = "single", selected = 1, target = 'row'),
+            width = "100%",
+            options = list(deferRender = TRUE, scroller = TRUE, dom = 't',
+                           autoWidth = FALSE#,
+                           # columnDefs = funs$aes$col_to_show(D_test, c(role_none, role_target))
+            ),
+            editable = FALSE
+        ) #%>%
+    # DT::formatRound(columns = intersect(type_numeric, role_input), digits = 0)
+    ## Wrap data frame in SharedData
+    output$unseen_observations <- DT::renderDataTable(expr = unseen_observations, server = TRUE)
+    DT::dataTableOutput('unseen_observations')
+
+    # Distribution Plot -------------------------------------------------------
     output$distPlot <- renderPlot({
 
         # generate bins based on input$bins from ui.R
