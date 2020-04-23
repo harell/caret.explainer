c26901b25d644705c59f95a507636564ff9afa62
harell <harel@loostiger.com>
2020-04-24 11:26:36 +1200

    delete all Rmd and md files

--- .app/tic/step_deploy_shiny.R
+++ .app/tic/step_deploy_shiny.R
@@ -8,6 +8,7 @@ DeployShiny <- R6::R6Class(
             write_requirements <- DeployShiny$funs$write_requirements
             load_app_config <- DeployShiny$funs$load_app_config
             env_var_exists <- DeployShiny$funs$env_var_exists
+            list_markdown <- DeployShiny$funs$list_markdown
             create_dir <- DeployShiny$funs$create_dir
 
             # Defensive Programming
@@ -25,6 +26,7 @@ DeployShiny <- R6::R6Class(
             fs::dir_copy(package_source, package_target)
             fs::dir_delete(file.path(package_target, "vignettes"))
             fs::dir_delete(file.path(package_target, "inst", "dashboard"))
+            fs::file_delete(list_markdown(package_target))
             write_requirements(package_target, dashboard_target)
 
             # Prepare Shiny
@@ -57,6 +59,7 @@ DeployShiny$funs <- new.env()
 
 DeployShiny$funs$env_var_exists = function(x) nchar(Sys.getenv(x)) > 0
 DeployShiny$funs$load_app_config = function() list2env(yaml::yaml.load_file(list.files(".", "config-shiny.yml", full.names = TRUE, recursive = TRUE)[1], eval.expr = TRUE), globalenv())
+DeployShiny$funs$list_markdown = function(path) list.files(path, ".(Rmd|md)$", full.names = TRUE, recursive = TRUE)
 DeployShiny$funs$create_dir = function(x){unlink(x, recursive = TRUE, force = TRUE); dir.create(x, FALSE, TRUE)}
 DeployShiny$funs$write_requirements <- function(package_path, dashboard_path){
     dependencies <-
