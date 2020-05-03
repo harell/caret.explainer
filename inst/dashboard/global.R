fba14fb534af471c7a13c122f7eb510fd7fd3ca4
harell <harel@loostiger.com>
2020-05-04 08:23:00 +1200

    R_CONFIG_ACTIVE

--- inst/dashboard/global.R
+++ inst/dashboard/global.R
@@ -22,7 +22,7 @@ datatable <- DT::datatable
 # Context Object ----------------------------------------------------------
 context <- new.env()
 ## Load dashboard config file
-list2env(config::get(config = "default", file = list.files(".", "config-shiny.yml$", recursive = TRUE, full.names = TRUE)[1]), envir = context)
+list2env(config::get(file = list.files(".", "config-shiny.yml$", recursive = TRUE, full.names = TRUE)[1]), envir = context)
 
 ## Default values
 context$values <- new.env()
