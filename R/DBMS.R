e740ee6438dd3f5ed443e2fbe5aaccddbd743346
harell <harel@loostiger.com>
2020-05-19 16:11:42 +1200

    create

--- R/DBMS.R
+++ R/DBMS.R
@@ -1,15 +1,27 @@
 # Database Management System ----------------------------------------------
 #' @title Database Management System for Interfacing between Presentation Layer and Functional Layer
+#' @field path (`character`) A path to the database location.
+#' @param artifact (`?`) An object.
+#' @param tags (`character`) A character vector with key:value tags.
 #' @export
 DBMS <- R6::R6Class(
     classname = "DBMS",
     cloneable = FALSE,
     lock_objects = FALSE,
     public = list(
+        # Public Fields --------------------------------------------------------
+        path = character(0),
         # Public Methods -------------------------------------------------------
+        #' @description
+        #' Initialize a database
+        #' @param path (`character`) A path to the database location
+        initialize = function(path = tempfile("DBMS")){self$path <- path},
         #' @description
         #' Establish a connection to the database
         establish_connection = function() DBMS$funs$establish_connection(self),
+        #' @description
+        #' Create an artifact in the archive
+        create = function(artifact, tags = c()) DBMS$funs$create(self, artifact, tags),
         #' @description
         #' Query the database by using JSON tags
         #' @param tags (`character`) A character vector with key:value tags
@@ -20,9 +32,17 @@ DBMS$funs <- new.env()
 
 # Public Methods ----------------------------------------------------------
 DBMS$funs$establish_connection <- function(self){
+    archivist::createLocalRepo(repoDir = self$path, force = FALSE, default = FALSE)
     invisible(self)
 }
 
-DBMS$funs$query_tags <- function(self, tags){
-    invisible(self)
+DBMS$funs$create <- function(self, artifact, tags){
+    archivist::saveToLocalRepo(artifact, repoDir = self$path, userTags = tags)
 }
+
+DBMS$funs$query_tags <- function(self, tags){
+    archivist::setLocalRepo(repoDir = self$path)
+    artifact <- archivist::asearch(tags, repo = NULL)
+    return(artifact)
+}
+
--- tests/testthat/test-DBMS.R
+++ tests/testthat/test-DBMS.R
@@ -3,13 +3,18 @@ context("unit test for DBMS")
 # Setup -------------------------------------------------------------------
 testthat::setup({
     assign("test_env", testthat::test_env(), envir = parent.frame())
+    options(path_archive = tempfile("DBMS"))
 })
 
 # General -----------------------------------------------------------------
 test_that("DBMS$new works", {
     attach(test_env)
-    expect_silent(database <- DBMS$new())
+    path_archive <- getOption("path_archive")
+
+    expect_silent(database <- DBMS$new(path_archive))
     expect_class(database, "DBMS")
+    expect_equal(database$path, path_archive)
+
     test_env$database <- database
 })
 
@@ -19,12 +24,18 @@ test_that("DBMS$establish_connection works", {
     expect_silent(test_env$database$establish_connection())
 })
 
-# import caret model ------------------------------------------------------
-test_that("DBMS$query_tags works", {
+# save_an_artifact --------------------------------------------------------
+test_that("DBMS$create works", {
     attach(test_env)
-    tags <- "class:learn"
-    expect_silent(test_env$database$query_tags(tags))
+    expect_silent(test_env$database$create(mtcars))
 })
+
+# # import caret model ------------------------------------------------------
+# test_that("DBMS$query_tags works", {
+#     attach(test_env)
+#     tags <- "class:data.frame"
+#     expect_class(test_env$database$query_tags(tags), "data.frame")
+# })
 
 
 
